c0dc96440b2d723c12e90fa2f6305638ef88b473
Agustin Arellano Monrroy <aaarellano@minsal.cl>
2024-07-02 11:30:54 -0400

    Implementación de q2_memory.py para contar los top 10 emojis más usados en los tweets

--- src/q2_memory.py
+++ src/q2_memory.py
@@ -1,4 +1,15 @@
-from typing import List, Tuple
+import json
+from collections import Counter
+import emoji
 
-def q2_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+def q2_memory(file_path: str):
+    counts = Counter()
+    
+    def extract_emojis(text: str):
+        return [char for char in text if char in emoji.EMOJI_DATA]
+    
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            counts.update(extract_emojis(json.loads(line)['content']))
+    
+    return counts.most_common(10)
