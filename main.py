3dc753579924176b0b6e5a4ac4d88e34f40db7d9
Agustin Arellano Monrroy <aaarellano@minsal.cl>
2024-07-02 11:36:56 -0400

    Añadido main.py para gestionar y ejecutar funciones de procesamiento de datos desde q1_time, q1_memory, q2_time, q2_memory, q3_time, q3_memory. Implementación de funciones para descargar y descomprimir archivos desde URL proporcionada.

---
+++ main.py
@@ -0,0 +1,52 @@
+import zipfile
+import os
+from typing import List, Tuple
+from src.q1_time import q1_time
+from src.q1_memory import q1_memory
+from src.q2_time import q2_time
+from src.q2_memory import q2_memory
+from src.q3_time import q3_time
+from src.q3_memory import q3_memory
+import gdown
+
+def descargar_y_descomprimir(url, ruta_destino):
+    local_filename = os.path.join(ruta_destino, 'tweets.zip')
+    os.makedirs(ruta_destino, exist_ok=True)
+    gdown.download(url, local_filename, quiet=False)
+    with zipfile.ZipFile(local_filename, 'r') as zip_ref:
+        zip_ref.extractall(ruta_destino)
+    for file_name in os.listdir(ruta_destino):
+        if file_name.endswith(".json"):
+            return os.path.join(ruta_destino, file_name)
+    return None
+
+def mostrar_resultados(nombre_funcion: str, resultados: List[Tuple]):
+    if nombre_funcion == "q1_time":
+        print(f"Resultados para q1_time: {resultados}")
+    elif nombre_funcion == "q1_memory":
+        print(f"Resultados para q1_memory: {resultados}")
+    elif nombre_funcion == "q2_time":
+        print(f"Resultados para q2_time: {resultados}")
+    elif nombre_funcion == "q2_memory":
+        print(f"Resultados para q2_memory: {resultados}")
+    elif nombre_funcion == "q3_time":
+        print(f"Resultados para q3_time: {resultados}")
+    elif nombre_funcion == "q3_memory":
+        print(f"Resultados para q3_memory: {resultados}")
+
+def principal():
+    url = 'https://drive.google.com/uc?id=1ig2ngoXFTxP5Pa8muXo02mDTFexZzsis'
+    ruta_destino = 'archivo/'
+    print("Descargando y descomprimiendo el archivo ZIP...")
+    archivo_json = descargar_y_descomprimir(url, ruta_destino)
+    if archivo_json is None:
+        print("No se encontró un archivo JSON en el ZIP.")
+        return
+
+    for func in [q1_time, q1_memory, q2_time, q2_memory, q3_time, q3_memory]:
+        nombre_funcion = func.__name__
+        result = func(archivo_json)
+        mostrar_resultados(nombre_funcion, result)
+
+if __name__ == "__main__":
+    principal()
